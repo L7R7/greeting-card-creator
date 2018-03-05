34f0fb988877179f7fbbb48947b85b5abf48173d
Leonhard Riedißer <leo008180@googlemail.com>
2018-03-05 22:29:53 +0100

    Add app page and router definitions

--- src/main/kotlin/com/l7r7/lab/greetingcard/creator/web/RouterDefinitions.kt
+++ src/main/kotlin/com/l7r7/lab/greetingcard/creator/web/RouterDefinitions.kt
@@ -1,9 +1,11 @@
 package com.l7r7.lab.greetingcard.creator.web
 
+import com.l7r7.lab.greetingcard.creator.card.domain.Card
 import com.l7r7.lab.greetingcard.creator.card.service.CardService
 import com.l7r7.lab.greetingcard.creator.card.service.ExternalCard
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 
@@ -12,5 +14,9 @@ class RouterDefinitions(private val cardService: CardService) {
     @Bean
     fun routes() = router {
         GET("/feed", { ServerResponse.ok().body(cardService.findAllPublishedExternal(), ExternalCard::class.java) })
+        GET("/count", { ServerResponse.ok().body(cardService.count(), Long::class.java) })
+        GET("/created", { ServerResponse.ok().body(cardService.findAllCreated(), Card::class.java) })
+        GET("/published", { ServerResponse.ok().body(cardService.findAllPublished(), Card::class.java) })
+        resources("/**", ClassPathResource("static/"))
     }
 }
